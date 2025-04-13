d2488862b6f3b85fad35e8a806253c40880d7626
Tsukasa OI <floss_rust@irq.a4lg.com>
2025-04-13 04:35:58 +0000

    RISC-V: OS-independent implication logic
    This commit adds the OS-independent extension implication logic for RISC-V.
    It implements:
    
    1.  Regular implication (A → B)
        a.  "the extension A implies the extension B"
        b.  "the extension A requires the extension B"
        c.  "the extension A depends on the extension B"
    2.  Extension group or shorthand (A == B1 & B2...)
        a.  "the extension A is shorthand for other extensions: B1, B2..."
        b.  "the extension A comprises instructions provided by B1, B2..."
        This is implemented as (A → B1 & B2... + B1 & B2... → A)
        where the former is a regular implication as required by specifications
        and the latter is a "reverse" implication to improve usability.
    
    and prepares for:
    
    3.  Implication with multiple requirements (A1 & A2... → B)
        a.  "A1 + A2 implies B"
        b.  (implicitly used to implement reverse implication of case 2)
    
    Although it uses macros and iterators, good optimizers turn the series of
    implications into fast bit-manipulation operations.
    
    In the case 2 (extension group or shorthand; where a superset extension
    is just a collection of other subextensions and provides no features by
    a superset itself), specifications do specify that an extension group
    implies its members but not vice versa.  However, implying an extension
    group from its members would improve usability on the feature detection
    (especially when the feature provider does not provide existence of such
    extension group but provides existence of its members).
    
    Similar "reverse implication" on RISC-V is implemented on LLVM.
    
    Case 3 is implicitly used to implement reverse implication of case 2 but
    there's another use case: implication with multiple requirements like
    "Zcf" and "Zcd" extensions (not yet implemented in this crate for now).
    
    To handle extension groups perfectly, we need to loop implication several
    times (until they converge; normally 2 times and up to 4 times when we add
    most of `riscv_hwprobe`-based features).
    To make implementation of that loop possible, `cache::Initializer` is
    modified to implement `PartialEq` and `Eq`.

---
+++ crates/std_detect/src/detect/os/riscv.rs
@@ -0,0 +1,91 @@
+//! Run-time feature detection utility for RISC-V.
+//!
+//! On RISC-V, full feature detection needs a help of one or more
+//! feature detection mechanisms (usually provided by the operating system).
+//!
+//! RISC-V architecture defines many extensions and some have dependency to others.
+//! More importantly, some of them cannot be enabled without resolving such
+//! dependencies due to limited set of features that such mechanisms provide.
+//!
+//! This module provides an OS-independent utility to process such relations
+//! between RISC-V extensions.
+
+use crate::detect::{Feature, cache};
+
+/// Imply features by the given set of enabled features.
+///
+/// Note that it does not perform any consistency checks including existence of
+/// conflicting extensions and/or complicated requirements.  Eliminating such
+/// inconsistencies is the responsibility of the feature detection logic and
+/// its provider(s).
+pub(crate) fn imply_features(mut value: cache::Initializer) -> cache::Initializer {
+    loop {
+        // Check convergence of the feature flags later.
+        let prev = value;
+
+        // Expect that the optimizer turns repeated operations into
+        // a fewer number of bit-manipulation operations.
+        macro_rules! imply {
+            // Regular implication:
+            // A1 => (B1[, B2...]), A2 => (B1[, B2...]) and so on.
+            ($($from: ident)|+ => $($to: ident)&+) => {
+                if [$(Feature::$from as u32),+].iter().any(|&x| value.test(x)) {
+                    $(
+                        value.set(Feature::$to as u32);
+                    )+
+                }
+            };
+            // Implication with multiple requirements:
+            // A1 && A2 ... => (B1[, B2...]).
+            ($($from: ident)&+ => $($to: ident)&+) => {
+                if [$(Feature::$from as u32),+].iter().all(|&x| value.test(x)) {
+                    $(
+                        value.set(Feature::$to as u32);
+                    )+
+                }
+            };
+        }
+        macro_rules! group {
+            ($group: ident == $($member: ident)&+) => {
+                // Forward implication as defined in the specifications.
+                imply!($group => $($member)&+);
+                // Reverse implication to "group extension" from its members.
+                // This is not a part of specifications but convenient for
+                // feature detection and implemented in e.g. LLVM.
+                imply!($($member)&+ => $group);
+            };
+        }
+
+        /*
+            If a dependency/implication is not explicitly stated in the
+            specification, it is denoted as a comment as follows:
+            "defined as subset":
+                The latter extension is described as a subset of the former
+                (but the evidence is weak).
+        */
+
+        imply!(zbc => zbkc); // defined as subset
+        group!(zkn == zbkb & zbkc & zbkx & zkne & zknd & zknh);
+        group!(zks == zbkb & zbkc & zbkx & zksed & zksh);
+        group!(zk == zkn & zkr & zkt);
+
+        group!(a == zalrsc & zaamo);
+
+        group!(b == zba & zbb & zbs);
+
+        imply!(zhinx => zhinxmin);
+        imply!(zdinx | zhinxmin => zfinx);
+
+        imply!(zfh => zfhmin);
+        imply!(q => d);
+        imply!(d | zfhmin => f);
+
+        imply!(zicntr | zihpm | f | zfinx => zicsr);
+        imply!(s | h => zicsr);
+
+        // Loop until the feature flags converge.
+        if prev == value {
+            return value;
+        }
+    }
+}
